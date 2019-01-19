7dbbd0dfadebafd344163c75f660327177e48ac3
William Suryawan <suryawan.william@gmail.com>
2019-01-19 19:08:56 +0700

    tambah file

---
+++ William-P0-W4-E7.js
@@ -0,0 +1,16 @@
+function urutkanAbjad(str) {
+  // you can only write your code here!
+  var kata = str.split("")
+  var balik =""
+  for (var i=0; i<kata.length; i++){
+    balik += kata.sort()[i]
+  }
+  return balik
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
